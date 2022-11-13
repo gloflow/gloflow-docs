f69aa23f1f7e76510c764921c4767164896e692d
Ivan Trajkovic <ivan.ebiz@gmail.com>
2022-11-13 22:10:39 +0100

    added creation of table_of_contents from rendered md files

--- ops/gf_build_docs.py
+++ ops/gf_build_docs.py
@@ -18,30 +18,62 @@
 import os, sys
 modd_str = os.path.abspath(os.path.dirname(__file__)) # module dir
 
+import jinja2
+
 #--------------------------------------------------
 def build():
     
     print("build docs")
 
-    build_dir_str = f"{modd_str}/../build"
+    table_of_contents_target_file_str = f"{modd_str}/../static/table_of_contents.html"
     docs_dirs_lst = [
-        (f"{modd_str}/../docs",     f"{modd_str}/../build"),
-        (f"{modd_str}/../docs/p2p", f"{modd_str}/../build/p2p")
+        ("main", f"{modd_str}/../docs",     f"{modd_str}/../static",     "/docs"),
+        ("p2p",  f"{modd_str}/../docs/p2p", f"{modd_str}/../static/p2p", "/docs/p2p")
     ]
 
-    for d_str, d_build_str in docs_dirs_lst:
+
+    sections_docs_html_files_map = {}
+    for section_name_str, d_str, d_target_str, url_base_str in docs_dirs_lst:
         
-        os.system(f"mkdir -p {d_build_str}")
+        os.system(f"mkdir -p {d_target_str}")
+        section_files_lst = []
 
         for f_str in os.listdir(d_str):
             if f_str.endswith(".md"):
+                
+                target_html_file_str = f"{f_str.split('.')[0]}.html"
+                target_html_file_path_str = f"{d_target_str}/{f_str.split('.')[0]}.html"
 
-                cmd_str = f"mkd2html {d_str}/{f_str} {d_build_str}/{f_str.split('.')[0]}.html"
+                cmd_str = f"mkd2html {d_str}/{f_str} {target_html_file_path_str}"
                 print(cmd_str)
 
                 os.system(cmd_str)
 
+                file_url_str = f"{url_base_str}/{target_html_file_str}"
+                section_files_lst.append(file_url_str)
 
+        sections_docs_html_files_map[section_name_str] = section_files_lst
+
+
+    build_docs_index(sections_docs_html_files_map,
+        table_of_contents_target_file_str)
+
+#--------------------------------------------------
+def build_docs_index(p_sections_docs_html_files_map,
+    p_table_of_contents_target_file_str):
+
+    f = open(f"{modd_str}/template/table_of_contents.html", "r")
+    template = jinja2.Template(f.read())
+    out_str  = template.render(sections_docs_html_files_map=p_sections_docs_html_files_map)
+
+    f.close()
+
+    f_target = open(p_table_of_contents_target_file_str, "w")
+    f_target.write(out_str)
+    f_target.close()
+
+    print(out_str)
+            
 #--------------------------------------------------
 if __name__ == "__main__":
     build()
